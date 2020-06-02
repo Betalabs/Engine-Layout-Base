260b48bc1aca00f472de58f5bb5709e641b6e16f
Victor Macedo <victor.macedo@betalabs.com.br>
2020-06-02 17:43:03 -0300

    Declare helpers functions file

---
+++ src/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('fonts')) {
+    function fonts(string $filename) {
+        return $filename;
+    }
+}
+
+if (!function_exists('images')) {
+    function images(string $filename) {
+        return $filename;
+    }
+}
+
+if (!function_exists('scripts')) {
+    function scripts(string $filename) {
+        return $filename;
+    }
+}
+
+if (!function_exists('styles')) {
+    function styles(string $filename) {
+        return $filename;
+    }
+}
