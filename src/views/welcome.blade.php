5f37b79604aabbf3ed095b4412fe79c9ea85985a
Victor Macedo <victor.macedo@betalabs.com.br>
2020-06-02 17:08:40 -0300

    Update app.php configuration and welcome message

--- src/views/welcome.blade.php
+++ src/views/welcome.blade.php
@@ -1,14 +1,36 @@
 <html>
-    <head>
-        <title>Engine Template</title>
-    </head>
+<head>
+    <title>Engine Template</title>
+</head>
     <body>
-        <center>
+        <div>
             <h1>Welcome</h1>
             <h2>Build something amazing!</h2>
-            <h4>
+            <h3>
                 <a href="/development/base">Start to develop</a>
-            </h4>
-        </center>
+            </h3>
+            <ol>
+                <li>
+                    <h4 style="margin: 0.5em;">
+                        Create configurations.json file to export to Engine-SelfLayout-Components
+                    </h4>
+                </li>
+                <li>
+                    <h4 style="margin: 0.5em;">
+                        Fill framework view configuration file at <em>/config/view.php</em>
+                    </h4>
+                </li>
+
+                <li>
+                    <h4 style="margin: 0.5em;">
+                        Create framework base directories
+                    </h4>
+                    <ol>
+                        <li><em>/storage/framework/views</em></li>
+                        <li><em>/storage/framework/logs</em></li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
     </body>
 </html>
