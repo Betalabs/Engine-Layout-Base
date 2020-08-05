855e88ab0e1030c2adb5b7cf51fb6f94030c9f3e
Victor Macedo <victor.macedo@betalabs.com.br>
2020-08-05 19:13:38 -0300

    Providing categories data provider and publishing seeder

--- src/app/DataProviders/Contracts/Ecommerce/CategoriesInterface.php
+++ src/app/DataProviders/Contracts/Ecommerce/CategoriesInterface.php
@@ -5,5 +5,10 @@ namespace EngineLayoutBase\App\DataProviders\Contracts\Ecommerce;
 
 interface CategoriesInterface
 {
-
+    /**
+     * Provides a collection with many categories ordered by depth as a tree.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection();
 }
--- src/app/DataProviders/Ecommerce/Categories.php
+++ src/app/DataProviders/Ecommerce/Categories.php
@@ -4,8 +4,12 @@ namespace EngineLayoutBase\App\DataProviders\Ecommerce;
 
 
 use EngineLayoutBase\App\DataProviders\Contracts\Ecommerce\CategoriesInterface;
+use EngineLayoutBase\App\Models\Category;
 
 class Categories implements CategoriesInterface
 {
-
+    public function collection()
+    {
+        return Category::query()->where('depth', 0)->get();
+    }
 }
